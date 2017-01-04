37abc122c19f38974555b287285c707bb650966d
BuildTools <theonlygusti@gmail.com>
2017-01-04 22:41:00 +0000

    Attempt at #18. Using new regex to parse values

--- plugin.py
+++ plugin.py
@@ -41,10 +41,9 @@ def seconds_to_text(seconds):
         formatted_text += str(seconds) + " " + "second" + ("s" if seconds > 1 else "")
     return formatted_text
 
-def parse_time_span(time_string):
+def parse_time_span(time_string, time_span_pattern):
     """Convert an inputted string representing a timespan, like 3h30m15s, into a duration in seconds."""
-    pattern = re.compile(r"^(?:(?P<hours>\d+)h)?(?:(?P<minutes>\d+)m)?(?:(?P<seconds>\d+)s)?$")
-    (hours, minutes, seconds) = pattern.match(time_string).groups()
+    (hours, minutes, seconds) = time_span_pattern.match(time_string).groups()
     hours = 0 if hours is None else int(hours)
     minutes = 0 if minutes is None else int(minutes)
     seconds = 0 if seconds is None else int(seconds)
@@ -160,11 +159,11 @@ def results(fields, original_query):
     message = " ".join(arguments[1:])
     with open("results.html") as html:
         # which input format is the user trying to use?
-        time_span_pattern = re.compile(r"^(?:(?P<hours>\d+)h)?(?:(?P<minutes>\d+)m)?(?:(?P<seconds>\d+)s)?$")
+        time_span_pattern = re.compile(r"^(?:(?P<hours>[0-9]+(?:[,.][0-9]+)?)h)?(?:(?P<minutes>[0-9]+(?:[,.][0-9]+)?)m)?(?:(?P<seconds>[0-9]+(?:[,.][0-9]+)?)s)?$")
         if time_span_pattern.match(time):
             try:
                 seconds = parse_time_span(time)
-                return results_dictionary("{} in {}".format(message or "Alarm", seconds_to_text(seconds)), [time, message or "{} alarm".format(seconds_to_text(seconds))], html.read())
+                return results_dictionary("{} in {}".format(message or "Alarm", seconds_to_text(seconds)), [time, message or "{} alarm".format(seconds_to_text(seconds)), time_span_pattern], html.read())
             except AttributeError:
                 return erroneous_results()
         else:
@@ -174,12 +173,11 @@ def results(fields, original_query):
             except ValueError:
                 return erroneous_results()
 
-def run(time, message):
+def run(time, message, time_span_pattern):
     seconds = None
     try:
-        time_span_pattern = re.compile(r"^(?:(?P<hours>\d+)h)?(?:(?P<minutes>\d+)m)?(?:(?P<seconds>\d+)s)?$")
         if time_span_pattern.match(time):
-            seconds = parse_time_span(time)
+            seconds = parse_time_span(time, time_span_pattern)
             show_notification("An alarm for {} was successfully set.".format(seconds_to_text(seconds)))
         else:
             seconds = seconds_until(parse_absolute_time(time))
